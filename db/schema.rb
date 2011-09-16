c23111b9012f766cf6bb1030c38c91cf7a92f47a
grentis <grentis@gmail.com>
2011-09-16 11:26:46 +0200

    merged with mago version

---
+++ db/schema.rb
@@ -0,0 +1,133 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20110916083844) do
+
+  create_table "application", :primary_key => "oid", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "status",     :limit => 3, :null => false
+    t.string   "label",                   :null => false
+  end
+
+  add_index "application", ["oid"], :name => "index_application_on_oid"
+
+  create_table "brand", :primary_key => "oid", :force => true do |t|
+    t.string "name", :null => false
+  end
+
+  add_index "brand", ["name"], :name => "index_brand_on_name", :unique => true
+  add_index "brand", ["oid"], :name => "index_brand_on_oid"
+
+  create_table "clab", :primary_key => "oid", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "status",            :limit => 3, :null => false
+    t.string   "label"
+    t.integer  "brand_oid",                      :null => false
+    t.integer  "application_oid",                :null => false
+    t.string   "country_isocode2",  :limit => 2, :null => false
+    t.string   "language_isocode2", :limit => 2, :null => false
+    t.string   "basicurl"
+    t.string   "applicationname"
+  end
+
+  add_index "clab", ["brand_oid", "application_oid", "country_isocode2", "language_isocode2"], :name => "index_clab_bacl", :unique => true
+  add_index "clab", ["oid"], :name => "index_clab_on_oid"
+
+  create_table "country", :id => false, :force => true do |t|
+    t.string "isocode2",       :limit => 2,  :null => false
+    t.string "isocode3",       :limit => 3
+    t.string "name"
+    t.string "englishname"
+    t.string "phoneprefix"
+    t.string "currency",       :limit => 25
+    t.string "currencysymbol", :limit => 5
+  end
+
+  add_index "country", ["isocode2"], :name => "index_country_on_isocode2", :unique => true
+
+  create_table "country_region", :id => false, :force => true do |t|
+    t.integer "country_isocode2", :null => false
+    t.integer "region_oid",       :null => false
+  end
+
+  add_index "country_region", ["country_isocode2", "region_oid"], :name => "index_country_region_on_country_isocode2_and_region_oid", :unique => true
+
+  create_table "group", :primary_key => "oid", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "groupname"
+    t.integer  "module_oid",    :null => false
+    t.integer  "visibilityoid", :null => false
+  end
+
+  add_index "group", ["oid"], :name => "index_group_on_oid"
+
+  create_table "group_clab", :id => false, :force => true do |t|
+    t.integer "group_oid", :null => false
+    t.integer "clab_oid",  :null => false
+  end
+
+  add_index "group_clab", ["group_oid", "clab_oid"], :name => "index_group_clab_on_group_oid_and_clab_oid", :unique => true
+
+  create_table "language", :id => false, :force => true do |t|
+    t.string "isocode2",    :limit => 2, :null => false
+    t.string "isocode3",    :limit => 3
+    t.string "name"
+    t.string "englishname"
+  end
+
+  add_index "language", ["isocode2"], :name => "index_language_on_isocode2", :unique => true
+
+  create_table "region", :primary_key => "oid", :force => true do |t|
+    t.integer "status",          :limit => 3, :null => false
+    t.string  "label",                        :null => false
+    t.integer "application_oid",              :null => false
+  end
+
+  add_index "region", ["oid"], :name => "index_region_on_oid"
+
+  create_table "user", :primary_key => "oid", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "username",                     :null => false
+    t.string   "password",                     :null => false
+    t.string   "email",                        :null => false
+    t.string   "firstname"
+    t.string   "lastname"
+    t.integer  "avatar_oid"
+    t.integer  "group_oid"
+    t.integer  "clab_oid"
+    t.boolean  "active",     :default => true, :null => false
+  end
+
+  add_index "user", ["oid"], :name => "index_user_on_oid"
+  add_index "user", ["username"], :name => "index_user_on_username", :unique => true
+
+  create_table "user_group", :id => false, :force => true do |t|
+    t.integer "user_oid",  :null => false
+    t.integer "group_oid", :null => false
+  end
+
+  add_index "user_group", ["user_oid", "group_oid"], :name => "index_user_group_on_user_oid_and_group_oid", :unique => true
+
+  create_table "worksession", :primary_key => "oid", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_oid"
+  end
+
+  add_index "worksession", ["oid"], :name => "index_worksession_on_oid"
+  add_index "worksession", ["user_oid"], :name => "index_worksession_on_user_oid"
+
+end
